335585c8d3f8d231cf2008f0bbba4ee92b814fb5
Matthias Hutsebaut <matthias@matthiasbook.local>
2017-03-04 21:15:51 +0100

    Removing all files in .gitignore

--- src/GogsBoard/App/Main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var AppModule_1 = require('./AppModule');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(AppModule_1.AppModule);
-//# sourceMappingURL=Main.js.map
